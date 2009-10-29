5073b14e8e0f9cf6a4123ffebd976a18d86d2498
Tristan Bereau <bereau@cmu.edu>
2009-10-29 14:31:23 +0000

    Added gyration package
    git-svn-id: svn+ssh://harry/home/tbereau/svn/peptideB@2 75246918-c341-11de-a42d-bdecacec1132

---
+++ packages/gyration/gyration.tcl
@@ -0,0 +1,48 @@
+# Apr. 16, 2009
+#
+# This script is intended to be used with peptideB.
+# It calculates the radius of gyration of existing PDB files
+# in the current folder.
+# 
+# Author : Tristan Bereau (bereau@cmu.edu)
+#
+#
+#
+
+# Description of the script :
+# It reads in PDB files, feeds the configuration in ESPResSo,
+# and output the radius of gyration.
+#
+#
+#
+
+
+
+# ----------------------------------- Script -------------------------------------- #
+
+# Turn the chemical resolution 'on'. This will help computing scalar products.
+set ::display_O_H "on"
+
+# Output file
+set output_file "gyration.dat"
+
+set files [glob *.pdb]
+
+set channel [open $output_file w]
+
+foreach pdb_file $files {
+
+	set coords [::peptideb::input::import_pdb $pdb_file]
+	::peptideb::espresso::create_topology $coords 0
+
+	analyze set chains 0 1 [expr [llength [lindex $coords 0]]/4]
+	set rad_g [analyze rg 0 1 [expr [llength [lindex $coords 0]]/4]]
+	
+	puts $channel [lindex $rad_g 0]
+	
+}
+
+
+
+
+
