02dd445ddeb552c0b0b3598f170723ce73a1bd52
Tristan Bereau <bereau@mpip-mainz.mpg.de>
2016-08-25 08:05:54 +0200

    forcecap for all but ljangle interactions

--- src/utils/warmup.tcl
+++ src/utils/warmup.tcl
@@ -4,7 +4,7 @@
 #
 
 
-namespace eval peptideb { 
+namespace eval peptideb {
     namespace eval utils {
 	# Warmup routine
 	# takes in argument the number of steps and times to call warmup
@@ -13,37 +13,37 @@ namespace eval peptideb {
 		set startcap 0.01
 		set capgoal  1000
 	    set usage "Usage : warmup steps times"
-	    	    
+
 	    if { $steps==0 || $times==0 } {
 		::mmsg::warn [namespace current] "Warmup steps are zero."
 		return
 	    }
-	    
+
 	    # Cap increment is determined here
 	    set capincr [expr ($capgoal - $startcap)/($times*1.)]
-	    
+
 	    # Set the initial force cap
 	    set cap $startcap
-	    
+
 	    for { set i 0 } { $i < $times } { incr i } {
 		::mmsg::debug [namespace current] "Warmup step $i of $times at time=[setmd time] (cap=$cap)."
-		inter ljforcecap $cap
+		inter forcecap $cap
 		if { $cap < $peptideb::HB_max_cap } {
 		    inter ljangleforcecap $cap
 		}
 		integrate $steps
 		set cap [expr $cap + $capincr]
-		
+
 		flush stdout
-	    } 
-	    
+	    }
+
 	    # Uncapping forces
 	    ::mmsg::send [namespace current] "Uncapping forces."
-	    inter ljforcecap 0
+	    inter forcecap 0
 	    # We keep a cap on the HBonds during the whole simulation.
 	    inter ljangleforcecap $peptideb::HB_max_cap
 	    ::mmsg::send [namespace current] "The HBond potential will be permanently capped at $peptideb::HB_max_cap"
-	    
+
             setmd time 0
 
 
@@ -51,4 +51,3 @@ namespace eval peptideb {
 	}
     }
 }
-
